e47ff09962b7b1be44644174e20e3ac76136e712
Tiffany Zheng <qz3@williams.edu>
2019-10-31 15:55:54 -0400

    view changes

---
+++ src/firestone/client/endpoints.clj
@@ -0,0 +1,28 @@
+(ns firestone.client.endpoints
+  (:require [clojure.string :refer [starts-with?]]
+            [clojure.data.json :refer [read-json write-str]]
+            [firestone.client.edn-api :refer [create-game!
+                                              end-turn!]]))
+
+
+
+(defn create-response
+  [response]
+  {:status  200
+   :headers {"Content-Type" "text/json; charset=utf-8"
+             "Access-Control-Allow-Origin"  "*"
+             "Access-Control-Allow-Methods" "*"}
+   :body    (write-str response)})
+
+(defn handler [request]
+
+  (let [uri (:uri request)]
+
+    (cond (starts-with? uri "/createGame")
+          (time (create-response (create-game!)))
+
+          (starts-with? uri "/endTurn")
+          (let [params (read-json (slurp (:body request)))
+                player-id (:playerId params)]
+            (create-response (end-turn! player-id))))))
+
